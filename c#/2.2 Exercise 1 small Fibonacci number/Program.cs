7c82b05cbbed838fe79e2f55099629377dbda35c
galya-study <galya.study@gmail.com>
2020-03-20 16:41:33 +0700

    add c# stepik exercise 2.2

---
+++ c#/2.2 Exercise 1 small Fibonacci number/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+        string nString;
+		nString = Console.ReadLine();
+		int n = Int32.Parse(nString);
+		
+		if ((n < 1) || (n > 40))
+		{
+			Console.WriteLine("{0}", n);
+		}
+		else
+		{
+			int[] f = new int[n+1];
+			f[0] = 0;
+			f[1] = 1;
+			
+			for (int i = 2; i < n+1; i++)
+			{
+				f[i] = f[i - 1] + f[i - 2];
+			}
+			
+			Console.WriteLine("{0}", f[n]);
+		}		
+	}
+}
