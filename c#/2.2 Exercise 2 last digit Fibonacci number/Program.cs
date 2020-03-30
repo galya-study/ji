eedc7c27e1cddce0ab93ab1153541cf83c8c86fe
galya-study <galya.study@gmail.com>
2020-03-30 16:04:16 +0700

    add 2.2 exercise 2

---
+++ c#/2.2 Exercise 2 last digit Fibonacci number/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		string nString;
+		nString = Console.ReadLine();
+		int n = Int32.Parse(nString);
+		
+		if ((n < 1) || (n > Math.Pow(10, 7)))
+		{
+			Console.WriteLine("{0}", n);
+		}
+		else
+		{
+			int F0 = 0;
+			int F1 = 1;
+			int FSum = (F0  + F1) % 10;
+			
+			F0 = F1;
+			F1 = FSum;
+			
+			for (int i = 3; i < n+1; i++)
+			{
+				FSum = (F0  + F1) % 10;
+
+				F0 = F1;
+				F1 = FSum;
+			}
+			
+			Console.WriteLine("{0}", FSum);
+		}		
+	}
+}
+//на вход - число n
+//на выход - последняя цифра в числе Фибонначи в позиции n
