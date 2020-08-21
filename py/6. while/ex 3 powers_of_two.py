5a68f3ac0cdfeffc9ba9f127a7036c63a20beb7b
galya-study <galya.study@gmail.com>
2020-08-21 17:15:28 +0700

    add new excercise py

--- py/6. while/ex 3 powers_of_two.py
+++ py/6. while/ex 3 powers_of_two.py
@@ -5,6 +5,22 @@
 # степени и саму степень.
 # Операцией возведения в степень пользоваться нельзя!
 
-import math
-
 N = int(input())
+
+power = 1
+value = 2
+
+if N == 1:
+    power = 0
+    value = 1
+    print(power, value)
+elif N == 2:
+    print(power, value)
+else:
+    while value <= N:
+        if value * 2 > N:
+            print(power, value)
+            break
+
+        value *= 2
+        power += 1
