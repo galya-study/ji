e4db8214be5645ee3d2f4ba942fa00a0f13070a5
galya-study <galya.study@gmail.com>
2020-08-31 12:41:31 +0700

    add new excercise py

--- py/7. lists/ex 13 num_equal_pairs.py
+++ py/7. lists/ex 13 num_equal_pairs.py
@@ -5,3 +5,12 @@
 # одну пару, которую необходимо посчитать.
 
 a_list = [int(i) for i in input().split()]
+count = 0
+
+for i in range(0, len(a_list)):
+    for j in range(0, len(a_list)):
+        if i != j and a_list[i] == a_list[j]:
+            count += 1
+count /= 2
+
+print(count)
