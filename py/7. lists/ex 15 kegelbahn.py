13a636907f1a846c7b39ec1ef0cb12153d9ed4be
galya-study <galya.study@gmail.com>
2020-08-31 12:57:45 +0700

    add new excercise py

--- py/7. lists/ex 15 kegelbahn.py
+++ py/7. lists/ex 15 kegelbahn.py
@@ -12,3 +12,14 @@
 # символов, где j-й символ есть “I”, если j-я кегля 
 # осталась стоять, или “.”, если j-я кегля была сбита.
 
+N, K = [int(i) for i in input().split()]
+
+a_list = ['I' for i in range(N)]
+
+for i in range(K):
+    l, r = [int(i) for i in input().split()]
+    for j in range(l - 1, r):
+        if a_list[j] != '.':
+            a_list[j] = '.'
+
+print(''.join(a_list))
