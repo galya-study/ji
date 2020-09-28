1239cd91d3ea0dfc861f7204ef664ab345f0e19e
galya-study <galya.study@gmail.com>
2020-09-28 21:24:49 +0700

    add new excercise py ВЫКУСИ Я СДЕЛАЛА ЭТОgit add .!

--- py/11. dicts/ex 10 sales.py
+++ py/11. dicts/ex 10 sales.py
@@ -1,25 +1,49 @@
 # Продажи
-# Дана база данных о продажах некоторого интернет-магазина. 
-# Каждая строка входного файла представляет собой запись 
-# вида Покупатель товар количество, где Покупатель — имя 
-# покупателя (строка без пробелов), товар — название товара 
-# (строка без пробелов), количество — количество приобретенных 
+# Дана база данных о продажах некоторого интернет-магазина.
+# Каждая строка входного файла представляет собой запись
+# вида Покупатель товар количество, где Покупатель — имя
+# покупателя (строка без пробелов), товар — название товара
+# (строка без пробелов), количество — количество приобретенных
 # единиц товара.
-# Создайте список всех покупателей, а для каждого покупателя 
-# подсчитайте количество приобретенных им единиц каждого вида 
-# товаров. Список покупателей, а также список товаров для 
+# Создайте список всех покупателей, а для каждого покупателя
+# подсчитайте количество приобретенных им единиц каждого вида
+# товаров. Список покупателей, а также список товаров для
 # каждого покупателя нужно выводить в лексикографическом порядке.
 
-buyer_set = set()
-buy_set = set()
-buyerbuy_count = dict()
-s = str(input())
-while s != None:
-    buyer, buy, count = [str(i) for i in s.split()]
-    buyer_set.add(buyer)
-    buy_set.add(buy)
-    if buyer + buy in buyerbuy_count:
-        buyerbuy_count[buyer + buy] = buyerbuy_count[buyer + buy] + int(count)
+from sys import stdin
+
+all_mas = []
+
+for line in stdin.readlines():
+    buyer, buy, count = [str(i) for i in line.split()]
+    flag = 0
+
+    if len(all_mas) == 0:
+        temp_dict = dict()
+        temp_dict[buy] = int(count)
+
+        temp_mas = [buyer, temp_dict]
+        all_mas.append(temp_mas)
+        flag = 1
     else:
-        buyerbuy_count[buyer + buy] = int(count)
-    s = str(input())
+        for i in range(len(all_mas)):
+            if all_mas[i][0] == buyer:
+                if buy in all_mas[i][1]:
+                    all_mas[i][1][buy] = all_mas[i][1][buy] + int(count)
+                else:
+                    all_mas[i][1][buy] = int(count)
+                flag = 1
+                break
+
+    if flag == 0:
+        temp_dict = dict()
+        temp_dict[buy] = int(count)
+
+        temp_mas = [buyer, temp_dict]
+        all_mas.append(temp_mas)
+
+for i in all_mas:
+    print(i[0], ':', sep='')
+    temp_dict = i[1]
+    for j in sorted(temp_dict.keys()):
+        print(j, temp_dict[j], sep=' ')
