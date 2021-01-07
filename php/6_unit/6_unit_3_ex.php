ad0e9a947d8a38d645eebfe66e0c8de216b3afda
galya-study <galya.study@gmail.com>
2021-01-07 22:23:19 +0700

    add file php

---
+++ php/6_unit/6_unit_3_ex.php
@@ -0,0 +1,21 @@
+<?php
+class Entree
+{
+    public $name;
+    public $ingredients = array();
+    public function __construct($name, $ingredients)
+    {
+        $this->name = $name;
+        $this->ingredients = $ingredients;
+    }
+
+    public function hasIngredient($ingredient)
+    {
+        return in_array($ingredient, $this->ingredients);
+    }
+}
+
+class PriceEntree extends Entree
+{
+    
+}
