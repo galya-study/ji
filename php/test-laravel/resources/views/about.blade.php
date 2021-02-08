a97e73955b3a99cb6ab718f740ec342af703835a
galya-study <galya.study@gmail.com>
2021-02-08 22:01:53 +0700

    edit file

---
+++ php/test-laravel/resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title')
+    Страница о нас
+@endsection
+
+@section('content')
+    <h1>Страница о нас</h1>
+@endsection
---
+++ php/test-laravel/resources/views/contact.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title')
+    Страница контактов
+@endsection
+
+@section('content')
+    <h1>Страница контактов</h1>
+@endsection
