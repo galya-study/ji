1c13109b3e5377b89ffd84df54ada697f1e44403
galya-study <galya.study@gmail.com>
2021-02-10 22:42:05 +0700

    edit file

--- php/test-laravel/resources/views/home.blade.php
+++ php/test-laravel/resources/views/home.blade.php
@@ -6,6 +6,7 @@
 
 @section('content')
     <h1>Главная страница</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nam nostrum corporis qui ipsum eveniet distinctio porro rerum temporibus a ducimus deserunt praesentium facere, numquam quas optio voluptatem et! Labore?</p>
 @endsection
 
 @section('aside')
---
+++ php/test-laravel/resources/views/inc/header.php
@@ -0,0 +1,8 @@
+<header class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+  <p class="h5 my-0 me-md-auto fw-normal"><a class="a-line" href="/">Ложки и плошки</a></p>
+  <nav class="my-2 my-md-0 me-md-3">
+    <a class="p-2 text-dark" href="/">Главная</a>
+    <a class="p-2 text-dark" href="/contact">Контакты</a>
+    <a class="p-2 text-dark" href="/about">О нас</a>
+  </nav>
+</header>
